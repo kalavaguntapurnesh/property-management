c3352ef5cbb81e97ea98361a08db894973be57ff
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-11-03 08:52:47 +0530

    Version 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
