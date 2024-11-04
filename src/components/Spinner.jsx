68ead4cdc323fe9f7216ebb8277cd4b991761da3
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-11-04 11:49:18 +0530

    One

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+const Spinner = () => {
+    return (
+      <div className="flex items-center justify-center z-9999 min-h-screen">
+        <div className="flex justify-center items-center h-screen">
+          <span class="loader"></span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Spinner;
+  
